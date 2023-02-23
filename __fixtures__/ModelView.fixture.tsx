5fe49b046bce195e8c284f85fe9c10744a04d1c3
sotnak <sotnak@email.cz>
2023-02-23 13:41:39 +0100

    using jail model in ModelView.fixture.tsx

--- __fixtures__/ModelView.fixture.tsx
+++ __fixtures__/ModelView.fixture.tsx
@@ -11,37 +11,33 @@ if(!env.token){
 
 export default {
     orbit: <ModelView style={{width: '99%', height: '98%', position: 'absolute'}}
-                        url={'http://109.123.202.213:3000/models/62556f89d41c1c0038689e4d'}
+                      url={'http://109.123.202.213:3000/models/6371826358b03a003a9de77d'}
                         requestHeaders={{'Authorization': env.token}}
     />,
     trackball: <ModelView style={{width: '99%', height: '98%', position: 'absolute'}}
                           controlsOption={ControlsOption.Trackball}
-                          url={'http://109.123.202.213:3000/models/62556f89d41c1c0038689e4d'}
+                          url={'http://109.123.202.213:3000/models/6371826358b03a003a9de77d'}
                           requestHeaders={{'Authorization': env.token}}
-    />,
-    jail: <ModelView style={{width: '99%', height: '98%', position: 'absolute'}}
-                      url={'http://109.123.202.213:3000/models/6371826358b03a003a9de77d'}
-                      requestHeaders={{'Authorization': env.token}}
     />,
     noStyle: <ModelView url={'http://109.123.202.213:3000/models/6371826358b03a003a9de77d'}
                      requestHeaders={{'Authorization': env.token}}
     />,
     lineCursor: <ModelView style={{width: '99%', height: '98%', position: 'absolute'}}
                            cursorOption={{style: CursorStyleOption.line}}
-                           url={'http://109.123.202.213:3000/models/62556f89d41c1c0038689e4d'}
+                           url={'http://109.123.202.213:3000/models/6371826358b03a003a9de77d'}
                            requestHeaders={{'Authorization': env.token}}
     />,
     sphereCursor: <ModelView style={{width: '99%', height: '98%', position: 'absolute'}}
-                           cursorOption={{style: CursorStyleOption.sphere}}
-                           url={'http://109.123.202.213:3000/models/62556f89d41c1c0038689e4d'}
-                           requestHeaders={{'Authorization': env.token}}
+                             cursorOption={{style: CursorStyleOption.sphere}}
+                             url={'http://109.123.202.213:3000/models/6371826358b03a003a9de77d'}
+                             requestHeaders={{'Authorization': env.token}}
     />,
     lineMoveCursor: <ModelView style={{width: '99%', height: '98%', position: 'absolute'}}
                                cursorOption={{
                                    style: CursorStyleOption.line,
                                    event: CursorEventOption.pointermove
                                }}
-                               url={'http://109.123.202.213:3000/models/62556f89d41c1c0038689e4d'}
+                               url={'http://109.123.202.213:3000/models/6371826358b03a003a9de77d'}
                                requestHeaders={{'Authorization': env.token}}
     />,
 }
