34cc1d95962dd03884fdfa1fcc3c5fa9d36bdcc0
sotnak <sotnak@email.cz>
2023-03-27 19:02:27 +0200

    ModelCompare

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import CompareViews from "./src/views/CompareViews";
 import ModelView from "./src/views/ModelView";
+import ModelCompare from "./src/views/ModelCompare"
 import {CursorStyleOption, CursorEventOption} from "./src/cursors/CursorOptions"
 import {ControlsOption} from "./src/builders/ControlsBuilder";
 import {CameraOption} from "./src/builders/CameraBuilder";
 
-export {CompareViews, ModelView, CursorStyleOption, CursorEventOption, ControlsOption, CameraOption}
+export {CompareViews, ModelView, ModelCompare, CursorStyleOption, CursorEventOption, ControlsOption, CameraOption}
