19d229dd1443fb2b61924b281c2ab2a38616a167
sotnak <sotnak@email.cz>
2023-03-27 15:25:49 +0200

    added camera option

--- index.ts
+++ index.ts
@@ -2,5 +2,6 @@ import CompareViews from "./src/views/CompareViews";
 import ModelView from "./src/views/ModelView";
 import {CursorStyleOption, CursorEventOption} from "./src/cursors/CursorOptions"
 import {ControlsOption} from "./src/builders/ControlsBuilder";
+import {CameraOption} from "./src/builders/CameraBuilder";
 
-export {CompareViews, ModelView, CursorStyleOption, CursorEventOption, ControlsOption}
+export {CompareViews, ModelView, CursorStyleOption, CursorEventOption, ControlsOption, CameraOption}
